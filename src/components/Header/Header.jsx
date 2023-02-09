aa2284748eddb51c49a312900a94d3d41b89a2ca
Connor <connordanielfrank@gmail.com>
2023-02-09 18:17:01 +1300

    dev: Style changes to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,14 @@ import styled from 'styled-components';
 import logo from './logo.png';
 
 const StyledHeader = styled.header`
-  background-color: var(--logo-color);
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BrandDiv = styled.div`
   display: flex;
   align-items: center;
-  `;
+`;
 
 const Logo = styled.img`
   width: 115px;
@@ -22,46 +26,78 @@ const H1 = styled.h1`
 `;
 
 const ContentDiv = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const UtilityDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
 `;
 
-const NavDiv = styled.div`
+const StyledNav = styled.nav`
+  display: flex;
+`;
+
+const StyledUl = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
 `;
 
 export default function Header() {
   return (
     <StyledHeader>
-      <Logo />
-      <H1>
-        New Zealand
-        <br />
-        Kennel Club
-      </H1>
+      <BrandDiv>
+        <Logo />
+        <H1>
+          New Zealand
+          <br />
+          Kennel Club
+        </H1>
+      </BrandDiv>
       <ContentDiv>
         <UtilityDiv>
-          <a href="#"><i /></a>
-          <a href="#"><i /></a>
-          <a href="#"><i /></a>
-          <a href="#"><i /></a>
-          <a href="#"><i /></a>
-          <a href="#"><i /></a>
+          <a href="#">
+            <i />
+            Event Search
+          </a>
+          <a href="#">
+            <i />
+            Find a Puppy
+          </a>
+          <a href="#">
+            <i />
+            Register Your Dog
+          </a>
+          <a href="#">
+            <i />
+            Shop
+          </a>
+          <a href="#">
+            <i />
+            NZKC TV
+          </a>
+          <a href="#">
+            <i />
+            Sign In
+          </a>
           <form action="#">
-            <input type="text" />
+            <input type="text" placeholder="Search" />
             <button type="submit"><i /></button>
           </form>
         </UtilityDiv>
-        <NavDiv>
-          <ul>
+        <StyledNav>
+          <StyledUl>
             <li>BREEDS A-Z</li>
             <li>EXPERT ADVICE</li>
             <li>PRODUCTS & SERVICES</li>
             <li>SPORTS & EVENTS</li>
             <li>CLUBS & DELEGATES</li>
-          </ul>
-        </NavDiv>
+          </StyledUl>
+        </StyledNav>
       </ContentDiv>
     </StyledHeader>
   );
