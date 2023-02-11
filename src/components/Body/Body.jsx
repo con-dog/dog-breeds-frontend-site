e40fb40a66beaa9d86ed976ec9ee3ad6bec3aaba
connor <connordanielfrank@gmail.com>
2023-02-12 11:58:25 +1300

    dev: Move breadcrumbs, add Main component

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Sidebar from './Sidebar/Sidebar';
-// import Main from './Main/Main';
+import Main from './Main/Main';
 
 const BodyDiv = styled.div`
   padding: 2rem;
@@ -12,129 +12,13 @@ const BodyDiv = styled.div`
   gap: 2rem;
 `;
 
-const StyledBreadcrumbs = styled.ul`
-  padding: 0;
-  width: 90%;
-  font-size: 1.2rem;
-  color: grey;
-  cursor: pointer;
-  margin: 2rem auto;
-  display: flex;
-  list-style: none;
-  & > li:not(:last-child) {
-    color: var(--utility--color);
-  }
-  & > li:not(:last-child)::after {
-    content: ' /';
-  }
-`;
-
-const StyledMain = styled.main`
-  background-color: #f1f1f1;
-  width: 100%;
-`;
-
 export default function Body() {
   return (
     <div>
-      <StyledBreadcrumbs>
-        <li>
-          <a>Home</a>
-        </li>
-        <li>
-          <a>Dog Breeds</a>
-        </li>
-      </StyledBreadcrumbs>
+
       <BodyDiv>
         <Sidebar />
-        <StyledMain>
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-          ;
-          <h1> Dog Breeds</h1>
-        </StyledMain>
+        <Main />
       </BodyDiv>
     </div>
   );
