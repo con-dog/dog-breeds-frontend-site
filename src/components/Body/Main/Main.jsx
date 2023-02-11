4cc15d8ba5b47360f3eff05784cb84c622f7e5e7
connor <connordanielfrank@gmail.com>
2023-02-11 18:39:07 +1300

    dev: Add Skeleton for Main Component

---
+++ src/components/Body/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import styled from 'styled-components';
+import { render } from '@testing-library/react';
+
+export default function Main() {
+  render(
+    <div>
+      {/* <Hero /> */}
+    </div>,
+  );
+}
