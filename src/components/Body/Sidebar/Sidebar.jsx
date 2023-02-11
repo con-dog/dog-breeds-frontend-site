36f32b4dada5f3ee8ab0e36df8742dde95cac26d
connor <connordanielfrank@gmail.com>
2023-02-12 11:57:58 +1300

    feat: Add breadcrumbs, add box shadow and padding

--- src/components/Body/Sidebar/Sidebar.jsx
+++ src/components/Body/Sidebar/Sidebar.jsx
@@ -3,8 +3,29 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
+const StyledBreadcrumbs = styled.ul`
+  padding: 0;
+  width: 90%;
+  font-size: 1.2rem;
+  color: grey;
+  cursor: pointer;
+  margin: 0 0 4rem 0;
+  display: flex;
+  list-style: none;
+  & > li:not(:last-child) {
+    color: var(--utility--color);
+  }
+  & > li:not(:last-child)::after {
+    content: ' /';
+  }
+`;
+
 const StyledAside = styled.aside`
-  width: 400px;
+  padding: 1.25rem;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px 0px;
+  width: 300px;
+  min-width: 300px;
   position: sticky;
   background-color: #FFF;
   align-self: flex-start;
@@ -115,65 +136,75 @@ const SubmitButton = styled.button`
 
 export default function Sidebar() {
   return (
-    <StyledAside>
-      <StyledLabelSelect htmlFor="breeds">
-        FIND BY BREED NAME
-      </StyledLabelSelect>
-      <SelectBreed name="breeds" id="breeds">
-        <option value="" disabled selected>Select a Breed</option>
-        <option value="Affenpinscher">Affenpinscher</option>
-        <option value="Afghan Hound">Afghan Hound</option>
-        <option value="Airedale Terrier">Airedale Terrier</option>
-        <option value="Akita">Akita</option>
-      </SelectBreed>
+    <div>
+      <StyledBreadcrumbs>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Dog Breeds</a>
+        </li>
+      </StyledBreadcrumbs>
+      <StyledAside>
+        <StyledLabelSelect htmlFor="breeds">
+          FIND BY BREED NAME
+        </StyledLabelSelect>
+        <SelectBreed name="breeds" id="breeds">
+          <option value="" disabled selected>Select a Breed</option>
+          <option value="Affenpinscher">Affenpinscher</option>
+          <option value="Afghan Hound">Afghan Hound</option>
+          <option value="Airedale Terrier">Airedale Terrier</option>
+          <option value="Akita">Akita</option>
+        </SelectBreed>
 
-      <form>
-        <FilterFormHeaderDiv>
-          <FilterFormHeader>FILTER BREEDS</FilterFormHeader>
-          <FilterButtonClearAll type="button">CLEAR ALL</FilterButtonClearAll>
-        </FilterFormHeaderDiv>
-        <hr />
-        <FormBodyDiv>
-          <AccordianButton type="button">
-            <span>GROUP</span>
-            <FontAwesomeIcon icon={faPlus} />
-          </AccordianButton>
-          <AccordianButton type="button">
-            <span>ACTIVITY LEVEL</span>
-            <FontAwesomeIcon icon={faPlus} />
-          </AccordianButton>
-          <AccordianButton type="button">
-            <span>BARKING LEVEL</span>
-            <FontAwesomeIcon icon={faPlus} />
-          </AccordianButton>
-          <AccordianButton type="button">
-            <span>CHARACTERISTICS</span>
-            <FontAwesomeIcon icon={faPlus} />
-          </AccordianButton>
-          <AccordianButton type="button">
-            <span>COAT TYPE</span>
-            <FontAwesomeIcon icon={faPlus} />
-          </AccordianButton>
-          <AccordianButton type="button">
-            <span>SHEDDING</span>
-            <FontAwesomeIcon icon={faPlus} />
-          </AccordianButton>
-          <AccordianButton type="button">
-            <span>SIZE</span>
-            <FontAwesomeIcon icon={faPlus} />
-          </AccordianButton>
-          <AccordianButton type="button">
-            <span>TRAINABILITY</span>
-            <FontAwesomeIcon icon={faPlus} />
-          </AccordianButton>
-        </FormBodyDiv>
-        <hr />
-        <SubmitDiv>
-          <SubmitButton type="button">
-            SUBMIT
-          </SubmitButton>
-        </SubmitDiv>
-      </form>
-    </StyledAside>
+        <form>
+          <FilterFormHeaderDiv>
+            <FilterFormHeader>FILTER BREEDS</FilterFormHeader>
+            <FilterButtonClearAll type="button">CLEAR ALL</FilterButtonClearAll>
+          </FilterFormHeaderDiv>
+          <hr />
+          <FormBodyDiv>
+            <AccordianButton type="button">
+              <span>GROUP</span>
+              <FontAwesomeIcon icon={faPlus} />
+            </AccordianButton>
+            <AccordianButton type="button">
+              <span>ACTIVITY LEVEL</span>
+              <FontAwesomeIcon icon={faPlus} />
+            </AccordianButton>
+            <AccordianButton type="button">
+              <span>BARKING LEVEL</span>
+              <FontAwesomeIcon icon={faPlus} />
+            </AccordianButton>
+            <AccordianButton type="button">
+              <span>CHARACTERISTICS</span>
+              <FontAwesomeIcon icon={faPlus} />
+            </AccordianButton>
+            <AccordianButton type="button">
+              <span>COAT TYPE</span>
+              <FontAwesomeIcon icon={faPlus} />
+            </AccordianButton>
+            <AccordianButton type="button">
+              <span>SHEDDING</span>
+              <FontAwesomeIcon icon={faPlus} />
+            </AccordianButton>
+            <AccordianButton type="button">
+              <span>SIZE</span>
+              <FontAwesomeIcon icon={faPlus} />
+            </AccordianButton>
+            <AccordianButton type="button">
+              <span>TRAINABILITY</span>
+              <FontAwesomeIcon icon={faPlus} />
+            </AccordianButton>
+          </FormBodyDiv>
+          <hr />
+          <SubmitDiv>
+            <SubmitButton type="button">
+              SUBMIT
+            </SubmitButton>
+          </SubmitDiv>
+        </form>
+      </StyledAside>
+    </div>
   );
 }
